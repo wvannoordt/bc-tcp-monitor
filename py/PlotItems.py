a6847a08d065407c7bae22fa7028c1db7a961096
Will van Noordt <willvanzero@gmail.com>
2020-01-27 11:34:54 +0000

    TS working

--- py/PlotItems.py
+++ py/PlotItems.py
@@ -13,6 +13,10 @@ class PlotViewable(ABC):
         pass
 
     @abstractmethod
+    def set_if_using_user_axis_limits(self):
+        pass
+
+    @abstractmethod
     def recieve_new_data(self, data_in):
         pass
 
@@ -40,7 +44,7 @@ class PlotCurveTimeSeries(PlotViewable):
         self.data_name = None
         self.is_initialized = False
         self.series_count = 0
-        self.max_time_history = 1000
+        self.max_time_history = 3000
         self.x_is_log = False
         #temporary
         self.y_is_log = True
@@ -48,26 +52,31 @@ class PlotCurveTimeSeries(PlotViewable):
         self.using_user_defined_limits = False
         self.axis_inflation = 1.25
         self.first_plot = True
+        self.xmax = 2
+        self.xmin = 1
+        self.ymin = math.inf
+        self.ymax = -math.inf
+        plt.ion()
+        self.no_increment_xmax = True
 
     def get_if_using_user_axis_limits(self):
         return self.using_user_defined_limits
 
+    def set_if_using_user_axis_limits(self, passed_value):
+        self.using_user_defined_limits = passed_value
+
     def auto_set_axis_bouds(self, figure, axis):
-        xmin = math.inf
-        xmax = -math.inf
-        ymin = math.inf
-        ymax = -math.inf
+        xmin = self.xmin
+        xmax = self.xmax
         for i in range(len(self.all_x_datas)):
-            xmin = min([xmin, min(self.all_x_datas[i])])
-            xmax = max([xmax, max(self.all_x_datas[i])])
-            ymin = min([ymin, min(self.all_y_datas[i])])
-            ymax = max([ymax, max(self.all_y_datas[i])])
+            self.ymin = min([self.ymin, min(self.all_y_datas[i])])
+            self.ymax = max([self.ymax, max(self.all_y_datas[i])])
         if xmin == xmax:
             xmin = xmax - 1
-        if ymin == ymax:
-            ymin = ymax - 1
-        delta_y = (ymax-ymin)*0.5
-        y_mean = (ymax+ymin)*0.5
+        if self.ymin == self.ymax:
+            self.ymin =self. ymax - 1
+        delta_y = (self.ymax-self.ymin)*0.5
+        y_mean = (self.ymax+self.ymin)*0.5
         axis.set_xlim(xmin, xmax)
         axis.set_ylim(y_mean - self.axis_inflation*delta_y, y_mean + self.axis_inflation*delta_y)
 
@@ -81,15 +90,23 @@ class PlotCurveTimeSeries(PlotViewable):
             if not self.is_initialized:
                 self.initialize_using_data(data_in)
             else:
+                if self.no_increment_xmax:
+                    self.no_increment_xmax = False
+                else:
+                    self.xmax = self.xmax + 1
+                    if self.xmax >= self.max_time_history:
+                        self.xmin = self.xmin + 1
                 for i in range(self.series_count):
                     if self.y_is_log:
                         self.all_y_datas[i].append(math.log10(data_in.data[i]))
                     else:
                         self.all_y_datas[i].append(data_in.data[i])
                     if len(self.all_x_datas[i]) <= self.max_time_history:
-                        self.all_x_datas[i].append(len(self.all_x_datas[i])+1)
+                        self.all_x_datas[i].append(self.xmax)
                     if len(self.all_y_datas[i]) > self.max_time_history:
                         self.all_y_datas[i].pop(0)
+                    if len(self.all_x_datas[i]) > self.max_time_history:
+                        self.all_x_datas[i].pop(0)
 
     def initialize_using_data(self, data_in):
         self.series_count = data_in.total_dim
@@ -111,17 +128,14 @@ class PlotCurveTimeSeries(PlotViewable):
                 cur_line, = axis.plot(self.all_x_datas[i],self.all_y_datas[i])
                 self.line_objects.append(cur_line)
             figure.canvas.draw_idle()
-            print("first done")
             self.first_plot = False
         else:
             for i in range(len(self.all_x_datas)):
-                print("{}, {}".format(len(self.all_x_datas[i])), len(self.all_y_datas[i]))
                 cur_line = self.line_objects[i]
                 cur_line.set_ydata(self.all_y_datas[i])
                 cur_line.set_xdata(self.all_x_datas[i])
-        figure.canvas.draw()
-        figure.canvas.flush_events()
-
+            figure.canvas.draw()
+            figure.canvas.flush_events()
 
     def depends_on(self, data_in):
         return data_in.name == self.data_name
